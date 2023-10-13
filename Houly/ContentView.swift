0a9e527b953d380311160cbaac09446ba443c9f3
AlJawharhALOtaibi <ALJawharhALOtaibi@gmail.com>
2023-10-13 03:12:51 +0300

    Initial Commit

---
+++ Houly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Houly
+//
+//  Created by AlJawharh AlOtaibi on 28/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Houly/HoulyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoulyApp.swift
+//  Houly
+//
+//  Created by AlJawharh AlOtaibi on 28/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HoulyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
